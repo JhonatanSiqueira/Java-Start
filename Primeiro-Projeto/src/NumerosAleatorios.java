16a416c6a61edb7dd543791013562cf1490bc319
Jhonatan Siqueira Pereira <79463243+JhonatanSiqueira@users.noreply.github.com>
2022-10-19 09:57:04 -0300

    Exercícios de LoopsComArrays

---
+++ Primeiro-Projeto/src/NumerosAleatorios.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+
+public class NumerosAleatorios {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[] numerosAleatorios = new int[20];
+
+        for(int i = 0; i < numerosAleatorios.length; i++){
+            int numero = random.nextInt(100);
+            numerosAleatorios[i] = numero;
+        }
+
+        System.out.println("Numeros aleatórios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.println(numero + " ");
+        }
+
+        
+        System.out.println("Numeros Sucessores: ");
+        for (int numero : numerosAleatorios) {
+            System.out.println((numero+1) + " ");
+        }
+
+        
+        System.out.println("Numeros antecessores: ");
+        for (int numero : numerosAleatorios) {
+            System.out.println((numero-1 ) + " ");
+        }
+    }
+}
---
+++ Primeiro-Projeto/src/OrdemInversa.java
@@ -0,0 +1,19 @@
+public class OrdemInversa {
+    public static void main(String[] args) {
+        
+        int [] vetor = {1, 2, 3, 4, 5, 6};
+
+        System.out.println("Vetor: ");
+        int count = 0;
+        while (count < vetor.length){
+            System.out.println(vetor[count] + " ");
+            count ++;
+        }
+
+        System.out.println("\nVetor inverso: ");
+        for(int i = (vetor.length -1); i >= 0; i--){
+            System.out.println(vetor[i] + " ");
+        }
+
+    }
+}
